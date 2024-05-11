a2573eede0ad062c3289df85e8843088ab639103
Martin Herrera <genkida_mh01@hotmail.com>
2024-05-11 18:38:36 -0300

    Actualizada entidad Oferta

--- Entities/Oferta.cs
+++ Entities/Oferta.cs
@@ -10,33 +10,43 @@ Esta clase establece la entidad "Oferta" y sus atributos
 namespace TruequeTools.Entities
 {
 
-	[Table("ofertas")]
+    [Table("ofertas")]
 
-	public class Oferta
-	{
+    public class Oferta
+    {
 
-		//ID UNIVOCO
+        //ID UNIVOCO
 
-		[Key]
-		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-		[Column("id")]
-		public int Id { get; set; }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+        public int Id { get; set; }
 
-		//DATOS INGRESADOS POR EL USUARIO
+        //DATOS INGRESADOS POR EL USUARIO
 
-		[Column("comentario")]
-		[MaxLength(50)]
-		public string? Comentario { get; set; }
+        [Column("productoDescripcion")]
+        [MaxLength(50)]
+        public string? ProductoDescripcion { get; set; }
 
-		[Column("usuarioId")]
-		public int UsuarioId { get; set; }
+        [Column("productoNombre")]
+        [MaxLength(50)]
+        public string? ProductoNombre { get; set; }
 
-		[Column("publicacionId")]
-		public int PublicacionId { get; set; }
+        [Column("productoFotoUrl")]
+        [MaxLength(255)]
+        public string? ProductoFotoUrl { get; set; }
 
-		public Usuario? Usuario { get; set; }
-		public Publicacion? Publicacion { get; set; }
+        //CATEGORIA ES IMPLICITA
 
-	}
+        [Column("usuarioId")]
+        public int UsuarioId { get; set; }
+
+        [Column("publicacionId")]
+        public int PublicacionId { get; set; }
+
+        public Usuario? Usuario { get; set; }
+        public Publicacion? Publicacion { get; set; }
+
+    }
 
 }
