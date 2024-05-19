a119e850b6d386a94cf223795dcd5ded5eeecb10
imartinezpass <imartinezpass@gmail.com>
2024-05-18 23:12:03 -0300

    aplico cambios de UI

--- Models/PreguntaViewModel.cs
+++ Models/PreguntaViewModel.cs
@@ -4,7 +4,7 @@ namespace TruequeTools.Models
 {
 	public class PreguntaViewModel
 	{
-		[Required(ErrorMessage = "Debe ingresar la pregunta")]
+		[Required(ErrorMessage = "La pregunta no puede estar vacía")]
 		[MinLength(10, ErrorMessage = "La pregunta debe tener al menos 10 caracteres")]
 		[Display(Name = "Escriba su pregunta")]
 		public string Pregunta { get; set; } = "";
