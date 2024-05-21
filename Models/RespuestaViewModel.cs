d55bc347ea846d8307f69dc8d57c4a0a45ed76ba
imartinezpass <imartinezpass@gmail.com>
2024-05-21 12:40:13 -0300

    agrego funcion responder (falta accion con la base de datos)

---
+++ Models/RespuestaViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TruequeTools.Models
+{
+	public class RespuestaViewModel
+	{
+		[Required(ErrorMessage = "La respuesta debe tener al menos 10 caracteres")]
+		[MinLength(10, ErrorMessage = "La respuesta debe tener al menos 10 caracteres")]
+		[Display(Name = "Escriba su respuesta")]
+		public string Respuesta { get; set; } = "";
+	}
+}
