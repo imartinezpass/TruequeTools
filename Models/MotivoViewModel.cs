b9af6890a9906f8f0640ddd4890b527b1857b18e
mayraPeguinho <may.peguinho0@gmail.com>
2024-06-06 20:06:49 -0300

    Actualizo el rechazo de ofertas, se agrega modal, y se carga motivo

---
+++ Models/MotivoViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TruequeTools.Models
+{
+    public class MotivoViewModel
+    {
+        [Required(ErrorMessage = "Debe seleccionar un motivo")]
+        [Display(Name = "Elija un motivo")]
+        public string Motivo { get; set; } = "";
+    }
+}
